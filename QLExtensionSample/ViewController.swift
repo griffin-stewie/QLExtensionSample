18ef87c29d5c745b37f33e48b43ce370b3c38af0
griffin-stewie <panterathefamilyguy@gmail.com>
2021-05-08 21:58:42 +0900

    Initial Commit

---
+++ QLExtensionSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QLExtensionSample
+//
+//  Created by griffin-stewie on 2021/05/08.
+//  
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
